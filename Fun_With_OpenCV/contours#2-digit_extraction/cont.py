9e1bf07068cec23301fded96e352265eefd81d5b
Deepak Singh <32263182+deepaksing@users.noreply.github.com>
2020-09-01 00:57:46 +0530

    Add files via upload
    added digit extraction to opencv contours

---
+++ Fun_With_OpenCV/contours#2-digit_extraction/cont.py
@@ -0,0 +1,33 @@
+import tensorflow as tf
+import numpy as np
+from tensorflow import keras
+import matplotlib.pyplot as plt
+import cv2
+
+img = cv2.imread('1.jpg')
+grayscalee = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+ret, threshold = cv2.threshold(grayscalee, 151, 255, cv2.THRESH_BINARY)
+
+grayscalee = cv2.GaussianBlur(grayscalee, (5, 5), 0)
+
+gaus = cv2.adaptiveThreshold(grayscalee, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 151, 6)
+
+contours,_ = cv2.findContours(gaus, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+idx = 0
+
+for contour in contours:
+	(x,y,w,h) = cv2.boundingRect(contour)
+	cv2.rectangle(gaus, (x,y), (x+w, y+h), (0, 255, 0), 2)
+	x,y,w,h = cv2.boundingRect(contour) 
+	if w>50 and h>50: 
+		idx+=1 
+		new_img=gaus[y:y+h,x:x+w] 
+		cv2.imwrite(str(idx) + '.png', new_img)
+		cv2.imshow('imsg', new_img)
+
+
+cv2.imshow('img', gaus)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
